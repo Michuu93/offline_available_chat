44a3b092b2c58b12111def969b10c4d654b0d8fd
Sylwia Üçüncü <sylwia_uc@yahoo.com>
2016-11-09 09:23:05 +0100

    (refactor) loadRooms function

--- server/src/Server.java
+++ server/src/Server.java
@@ -1,34 +1,43 @@
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
-public class Server{
+public class Server {
 
     static ArrayList<ObjectOutputStream> outputStreams;
+    ArrayList<String> chatRoomsList = new ArrayList<String>();
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
         connect();
     }
 
-    static private void connect(){
+    static private void connect() {
         try {
             ServerSocket serverSocket = new ServerSocket(9001);
 
-            while(true) {
+            while (true) {
                 Socket clientSocket = serverSocket.accept();
                 ObjectOutputStream outStream = new ObjectOutputStream(clientSocket.getOutputStream());
                 outputStreams.add(outStream);
                 Thread thread = new Thread(new ClientService(clientSocket));
                 thread.start();
             }
-        }catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
 
-    private void generateRoomsList(){
-        File file = new File ("chatRooms.txt");
+    private void loadRooms() {
+        try {
+            File file = new File("chatRooms.txt");
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                chatRoomsList.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
 }
