e3c52ac1679798ec76a4cd0fa2b3a33f477ea4a6
Michał H <michuu93@gmail.com>
2016-11-11 19:04:18 +0100

    Disconnecting

--- client/src/sample/WriterThread.java
+++ client/src/sample/WriterThread.java
@@ -6,14 +6,12 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 
 public class WriterThread implements Runnable {
-    private ObjectOutputStream writer;
 
     public WriterThread(MessagePacket message) {
             try {
-                writer = new ObjectOutputStream(Main.getConnection().getSocket().getOutputStream());
+                ObjectOutputStream writer = new ObjectOutputStream(Main.getConnection().getSocket().getOutputStream());
                 writer.writeObject(message);
                 writer.flush();
-                //writer.close();
                 System.out.println("Wysłano do pokoju: " + message.getRoom() + "\nwiadomość: " + message.getMessage());
 
                 //zamykanie wątku, nie wiem czy działa
