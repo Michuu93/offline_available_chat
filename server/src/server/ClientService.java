e41f617fc51d07eabe1521eb08842e173e7c648e
Sylwia Üçüncü <sylwia_uc@yahoo.com>
2016-11-11 21:58:37 +0100

    (fix) send to all clients and read messages

--- server/src/server/ClientService.java
+++ server/src/server/ClientService.java
@@ -1,41 +1,40 @@
 package server;
 
 import common.MessagePacket;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
+
+import java.io.*;
+import java.util.ArrayList;
 
 public class ClientService implements Runnable{
 
-    private ObjectInputStream inStream;
-    private Server server = new Server();
+    private ObjectInputStream reader;
+    private ArrayList<Client> clients;
 
-    public ClientService (Socket socket){
+    public ClientService (Client client){
         try{
-            Socket clientSocket = socket;
-            inStream = new ObjectInputStream(clientSocket.getInputStream());
+            reader = new ObjectInputStream(client.getSocket().getInputStream());
         }catch (Exception ex){
             ex.printStackTrace();
         }
     }
     @Override
     public void run() {
-        read();
+            read();
     }
 
     private void read(){
         Object object;
-
-        while (true) {
+        Boolean complete = true;
+        while (complete) {
             try {
-                if ((object = inStream.readObject()) != null){
+                if ((object = reader.readObject()) != null) {
                     MessagePacket messagePacket = (MessagePacket) object;
                     System.out.println("Read message from client: " + messagePacket.getRoom() + ": " + messagePacket.getMessage());
-                    sendToAll(messagePacket);
-                    serialize(messagePacket);
+                    Server.sendToAll(messagePacket);
+                    //serialize(messagePacket);
                 }
+            }catch (EOFException e){
+                complete = false;
             } catch (IOException e) {
                 e.printStackTrace();
             } catch (ClassNotFoundException e) {
@@ -44,18 +43,6 @@ public class ClientService implements Runnable{
         }
     }
 
-    private void sendToAll(Object message) {
-        System.out.println("sendToAll method");
-        for (Client client: server.clients) {
-            try{
-                ObjectOutputStream outStream = client.getClientOutputStream();
-                System.out.println("Writing to all: " + message);
-                outStream.writeObject(message);
-            }catch(Exception ex){
-                ex.printStackTrace();
-            }
-        }
-    }
 
     private void serialize(Object packet){
         try {
