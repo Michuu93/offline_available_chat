c3a118be8740a4db2f7e1e8f9043534f52f945c4
Sylwia Üçüncü <sylwia_uc@yahoo.com>
2016-11-12 16:02:25 +0100

    (feat) UsersPacket with chat rooms name and list of users nicknames

---
+++ common/src/common/UsersPacket.java
@@ -0,0 +1,34 @@
+package common;
+
+import java.util.ArrayList;
+
+public class UsersPacket {
+
+    private String room;
+    private ArrayList<String> clientsList =  new ArrayList<String>();
+
+    public UsersPacket(String room, ArrayList<String> clientsList) {
+        this.room = room;
+        this.clientsList = clientsList;
+    }
+
+    public UsersPacket(){
+
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public ArrayList<String> getClientsList() {
+        return clientsList;
+    }
+
+    public void setClientsList(ArrayList<String> clientsList) {
+        this.clientsList = clientsList;
+    }
+}
